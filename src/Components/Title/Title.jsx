9ceb3d0442afb01dff48b18c805894919778a6c9
IbrahimMohram1 <ibrahimmohram7@gmail.com>
2025-10-21 06:00:37 +0300

    Update hero section

--- src/Components/Title/Title.jsx
+++ src/Components/Title/Title.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 export default function Title({title}) {
   return (
     <div>
-      <div className="flex items-center justify-center  py-8 my-5">
+      <div className="flex items-center justify-center  py-8 my-5 m-auto overflow-hidden">
       <div className="flex flex-col gap-4">
         <div className="w-10 h-0.5 bg-blue-400 transform -rotate-45 origin-bottom-left"></div>
         <div className="w-10 h-0.5 bg-blue-400 transform rotate-45 origin-top-left"></div>
       </div>
       
-      <h2 className="text-5xl font-bold text-[#5B8DEF]">{title}</h2>
+      <h2 className="md:text-5xl text-2xl text-center text-wrap font-bold text-[#5B8DEF]">{title}</h2>
       
-     <div className="flex flex-col gap-6 ml-12">
+     <div className="flex flex-col gap-6 md:ml-12 ml-8">
         <div className="w-10 h-0.5 bg-blue-400 transform -rotate-140 origin-bottom-left"></div>
         <div className="w-10 h-0.5 bg-blue-400 transform rotate-140 origin-top-left"></div>
       </div>
